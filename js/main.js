107845acec367ca67303a319aaede6f207bdaa7d
“MarisolenlaWeb” <marcarrimor@gmail.com>
2017-02-10 17:53:20 -0600

    Pruebas con slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ jQuery(document).ready(function(){
 		hoverClass: 'sfHover'
 	});
 });
+
+jQuery(document).ready(function($) {
+    $('#un-carousel').owlCarousel({
+    	"loop":true,
+    	"nav":true,
+    	"dots":false,
+    	"items":1,
+    	"autoHeight":true,
+    	"navText":["Previous","Next"]
+    }); 
+});
+
+$(document).ready(function(){
+  $("#un-carousel").owlCarousel();
+});

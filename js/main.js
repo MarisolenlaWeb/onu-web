6dd8341c1fcc2472b8b8307864d64d786dbdd203
Marisol Carrillo <marbook@MacBook-Air.local>
2017-02-12 20:02:51 -0600

    Se agregó slider

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
 
-jQuery(document).ready(function(){
-	jQuery('ul.sf-menu').superfish({
-		hoverClass: 'sfHover'
-	});
-});
+// jQuery(document).ready(function(){
+// 	jQuery('ul.sf-menu').superfish({
+// 		hoverClass: 'sfHover'
+// 	});
+// });
 
 jQuery(document).ready(function($) {
-    $('#un-carousel').owlCarousel({
-    	"loop":true,
-    	"nav":true,
-    	"dots":false,
-    	"items":1,
-    	"autoHeight":true,
-    	"navText":["Previous","Next"]
-    }); 
-});
-
-$(document).ready(function(){
-  $("#un-carousel").owlCarousel();
+    // $('.owl-carousel').owlCarousel({
+    // 	"loop":true,
+    // 	"nav":true,
+    // 	"dots":false,
+    // 	"items":1,
+    // 	"autoHeight":true,
+    // 	"navText":["Previous","Next"]
+    // }); 
+    $('.owl-carousel').owlCarousel({
+	    loop:true,
+	    nav:false,
+	    dots:true,
+	    items:1,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+	})
 });
